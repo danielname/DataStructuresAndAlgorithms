1824120edefead836a414b57afebecdaeaa47a5e
Daniel Name <daniel.v.name@gmail.com>
2023-02-14 23:06:39 -0600

    New Code - Completed Append Exercise

--- src/main/java/datastructures/linkedlist/Main.java
+++ src/main/java/datastructures/linkedlist/Main.java
@@ -1,12 +1,13 @@
 package datastructures.linkedlist;
 
-import java.util.LinkedList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(4);
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.makeEmpty();
+        myLinkedList.append(1);
+        myLinkedList.append(2);
 
         myLinkedList.getHead();
         myLinkedList.getTail();
@@ -16,16 +17,17 @@ public class Main {
         myLinkedList.printList();
 
         /*
-            EXPECTED OUTPUT:
-            ----------------
-            Head: 4
-            Tail: 4
-            Length: 1
+       		EXPECTED OUTPUT:
+        	----------------
+        	Head: 1
+        	Tail: 2
+        	Length: 2
 
-            Linked List:
-            4
+        	Linked List:
+        	1
+        	2
 
-        */
+     	*/
 
     }
 
