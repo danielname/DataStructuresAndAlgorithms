22cd99861e9d74c54abeeb753007b4ce8f974366
Daniel Name <daniel.v.name@gmail.com>
2023-02-20 11:02:37 -0600

    New Code - DLL Completed Remove Last Exercise

--- src/main/java/datastructures/doublylinkedlist/Main.java
+++ src/main/java/datastructures/doublylinkedlist/Main.java
@@ -7,23 +7,20 @@ public class Main {
         DoublyLinkedList myDLL = new DoublyLinkedList(1);
         myDLL.append(2);
 
-        myDLL.getHead();
-        myDLL.getTail();
-        myDLL.getLength();
+        // (2) Items - Returns 2 Node
+        System.out.println(myDLL.removeLast().value);
+        // (1) Item - Returns 1 Node
+        System.out.println(myDLL.removeLast().value);
+        // (0) Items - Returns null
+        System.out.println(myDLL.removeLast());
 
-        System.out.println("\nDoubly Linked List:");
-        myDLL.printList();
 
-        /*
-       		EXPECTED OUTPUT:
+    	/*
+        	EXPECTED OUTPUT:
         	----------------
-        	Head: 1
-        	Tail: 2
-        	Length: 2
-
-        	Doubly Linked List:
-        	1
         	2
+        	1
+        	null
 
      	*/
 
