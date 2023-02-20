251be49bf50b69f2b7a653c7739d0e9c430ed00c
Daniel Name <daniel.v.name@gmail.com>
2023-02-20 10:45:26 -0600

    New Code - DLL Append Exercise Complete

--- src/main/java/datastructures/doublylinkedlist/Main.java
+++ src/main/java/datastructures/doublylinkedlist/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        DoublyLinkedList myDLL = new DoublyLinkedList(1);
+        myDLL.append(2);
 
         myDLL.getHead();
         myDLL.getTail();
@@ -14,16 +15,17 @@ public class Main {
         myDLL.printList();
 
         /*
-            EXPECTED OUTPUT:
-            ----------------
-            Head: 7
-            Tail: 7
-            Length: 1
+       		EXPECTED OUTPUT:
+        	----------------
+        	Head: 1
+        	Tail: 2
+        	Length: 2
 
-            Doubly Linked List:
-            7
+        	Doubly Linked List:
+        	1
+        	2
 
-        */
+     	*/
 
     }
 
