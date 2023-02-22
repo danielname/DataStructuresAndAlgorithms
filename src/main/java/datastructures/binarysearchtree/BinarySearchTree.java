a15f076b45c7e9c20c8b49fcbb5d7f9247acef28
Daniel Name <daniel.v.name@gmail.com>
2023-02-22 12:37:16 -0600

    New Code - BST - Completed Constructor Exercise

---
+++ src/main/java/datastructures/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,29 @@
+package datastructures.binarysearchtree;
+
+
+public class BinarySearchTree {
+
+    class Node{
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+    Node root;
+
+    BinarySearchTree(){
+        root = null;
+    }
+
+    // CREATE CLASS VARIABLE (ROOT) AND NODE CLASS HERE//
+    //                                                 //
+    //                                                 //
+    //                                                 //
+    //                                                 //
+    /////////////////////////////////////////////////////
+
+}
