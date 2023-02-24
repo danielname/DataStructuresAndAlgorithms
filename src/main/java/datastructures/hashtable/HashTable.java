e90faa2019ed72eaca0a0317a9644652cab05b34
Daniel Name <daniel.v.name@gmail.com>
2023-02-24 13:17:20 -0600

    New Code - HT - Completed Constructor Exercise

---
+++ src/main/java/datastructures/hashtable/HashTable.java
@@ -0,0 +1,42 @@
+package datastructures.hashtable;
+
+public class HashTable {
+
+    private int size = 7;
+    private Node[] dataMap;
+
+    public HashTable(){
+        dataMap = new Node[size];
+    }
+
+    class Node{
+        String key;
+        int value;
+        Node next;
+        Node(String key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE//
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    //                                                         //
+    /////////////////////////////////////////////////////////////
+
+    public void printTable() {
+        for (int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            if(dataMap[i] != null) {
+                Node temp = dataMap[i];
+                while (temp != null) {
+                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+
+}
