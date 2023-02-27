51b19b4b5efe0149817df2df448b7f4bc99583dc
Daniel Name <daniel.v.name@gmail.com>
2023-02-27 11:21:54 -0600

    followed code for hash exercise

--- src/main/java/datastructures/hashtable/HashTable.java
+++ src/main/java/datastructures/hashtable/HashTable.java
@@ -1,42 +1,53 @@
 package datastructures.hashtable;
 
-public class HashTable {
 
+public class HashTable {
     private int size = 7;
     private Node[] dataMap;
 
-    public HashTable(){
-        dataMap = new Node[size];
-    }
-
-    class Node{
+    class Node {
         String key;
         int value;
         Node next;
-        Node(String key, int value){
+
+        Node(String key, int value) {
             this.key = key;
             this.value = value;
         }
     }
 
-    // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE//
-    //                                                         //
-    //                                                         //
-    //                                                         //
-    //                                                         //
-    /////////////////////////////////////////////////////////////
+    public HashTable() {
+        dataMap = new Node[size];
+    }
 
     public void printTable() {
         for (int i = 0; i < dataMap.length; i++) {
             System.out.println(i + ":");
-            if(dataMap[i] != null) {
-                Node temp = dataMap[i];
-                while (temp != null) {
-                    System.out.println("   {" + temp.key + ", " + temp.value + "}");
-                    temp = temp.next;
-                }
+            Node temp = dataMap[i];
+            while (temp != null) {
+                System.out.println("   {" + temp.key + "= " + temp.value + "}");
+                temp = temp.next;
             }
         }
     }
+
+    // HASH METHOD MUST BE PUBLIC FOR CODE IN MAIN TO WORK
+
+    public int hash(String key){
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++){
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
+    }
+
+    /// WRITE HASH METHOD HERE ///
+    //                          //
+    //                          //
+    //                          //
+    //                          //
+    //////////////////////////////
 
 }
