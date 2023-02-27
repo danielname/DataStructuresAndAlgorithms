082130d7a36aae09c7aac9ab78221a5704d34b84
Daniel Name <daniel.v.name@gmail.com>
2023-02-27 12:56:06 -0600

    New Code - HT - Completed Get Exercise

--- src/main/java/datastructures/hashtable/Main.java
+++ src/main/java/datastructures/hashtable/Main.java
@@ -9,26 +9,21 @@ public class Main {
         myHashTable.set("nails", 100);
         myHashTable.set("tile", 50);
         myHashTable.set("lumber", 80);
-        myHashTable.set("bolts", 200);
-        myHashTable.set("screws", 140);
 
-        myHashTable.printTable();
+        System.out.println("Lumber:");
+        System.out.println( myHashTable.get("lumber") );
+
+        System.out.println("\nBolts:");
+        System.out.println( myHashTable.get("bolts") );
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            0:
-            1:
-            2:
-            3:
-               {screws= 140}
-            4:
-               {bolts= 200}
-            5:
-            6:
-               {nails= 100}
-               {tile= 50}
-               {lumber= 80}
+            Lumber:
+            80
+
+            Bolts:
+            0
 
         */
 
