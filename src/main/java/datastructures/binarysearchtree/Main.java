40eabe9a2cf16113a6fc36591c688fb15de8cadd
Daniel Name <daniel.v.name@gmail.com>
2023-03-30 17:07:59 -0500

    New Code - DFS - Completed DFS In Order Exercise

--- src/main/java/datastructures/binarysearchtree/Main.java
+++ src/main/java/datastructures/binarysearchtree/Main.java
@@ -14,14 +14,14 @@ public class Main {
         myBST.insert(52);
         myBST.insert(82);
 
-        System.out.println("\nDFS PostOrder:");
-        System.out.println( myBST.DFSPostOrder() );
+        System.out.println("\nDFS InOrder:");
+        System.out.println( myBST.DFSInOrder() );
 
         /*
             EXPECTED OUTPUT:
             ----------------
-            DFS PostOrder:
-            [18, 27, 21, 52, 82, 76, 47]
+            DFS InOrder:
+            [18, 21, 27, 47, 52, 76, 82]
 
         */
 
